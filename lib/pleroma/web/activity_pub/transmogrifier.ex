13ac6409c31d4a08dc7c87445bb4b8532e0be109
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2020-09-25 15:52:48 +0200

    Move Utils json-ld header and date to Builder

--- lib/pleroma/web/activity_pub/transmogrifier.ex
+++ lib/pleroma/web/activity_pub/transmogrifier.ex
@@ -667,7 +667,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
     data =
       data
       |> Map.put("object", object)
-      |> Map.merge(Utils.make_json_ld_header())
+      |> Map.merge(Builder.json_ld_header())
       |> Map.delete("bcc")
 
     {:ok, data}
@@ -688,7 +688,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
     data =
       data
       |> strip_internal_fields
-      |> Map.merge(Utils.make_json_ld_header())
+      |> Map.merge(Builder.json_ld_header())
       |> Map.delete("bcc")
 
     {:ok, data}
@@ -708,7 +708,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
       data =
         data
         |> Map.put("object", object)
-        |> Map.merge(Utils.make_json_ld_header())
+        |> Map.merge(Builder.json_ld_header())
 
       {:ok, data}
     end
@@ -726,7 +726,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
       data =
         data
         |> Map.put("object", object)
-        |> Map.merge(Utils.make_json_ld_header())
+        |> Map.merge(Builder.json_ld_header())
 
       {:ok, data}
     end
@@ -737,7 +737,7 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
       data
       |> strip_internal_fields
       |> maybe_fix_object_url
-      |> Map.merge(Utils.make_json_ld_header())
+      |> Map.merge(Builder.json_ld_header())
 
     {:ok, data}
   end
